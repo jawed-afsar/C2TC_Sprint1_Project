d2e4085ef8a48f7ddc4d4ee2753bb0ee3296c508
jawed <jawed@LAPTOP-BTOBCIRH>
2025-11-07 22:53:16 +0530

    updated config

--- Order/src/main/java/com/tnsif/Order/config/OrderConfig.java
+++ Order/src/main/java/com/tnsif/Order/config/OrderConfig.java
@@ -1,16 +1,25 @@
 package com.tnsif.Order.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class OrderConfig implements WebMvcConfigurer {
+@CrossOrigin(origins = "http://localhost:3000")  // React connection
+public class OrderConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // or specify frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
